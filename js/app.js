8783a52743569243f373fa21874510f673cb1174
Nacho Coloma <icoloma@extrema-sistemas.com>
2014-08-21 11:55:40 +0200

    New release

--- js/app.js
+++ js/app.js
@@ -8227,7 +8227,7 @@ exports.communities=[{"className":"GDG-Spain","name":"GDG Spain","url":"http://w
   })
 
 })(jQuery, window, window.document)
-}).call(this,require("oMfpAn"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_8faca963.js","/")
+}).call(this,require("oMfpAn"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_48194be8.js","/")
 },{"./communities":6,"./talks/agenda.js":8,"./vendor/foundation/foundation":11,"./vendor/foundation/foundation.interchange":10,"./vendor/foundation/foundation.tab":12,"./vendor/foundation/foundation.topbar":13,"buffer":1,"lodash":5,"oMfpAn":4}],8:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
 (function() {
